65466df8c1e65af5a0d4d47ba8aea10c8981cd2f
Avinash Toppo <atoppo267@gmail.com>
2021-03-13 05:16:26 +0530

    Updates

---
+++ precache-manifest.b28cb60954a29227d278b6053fb70a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c7047a4484865b2e094e5745da3602",
+    "url": "/react-ENablex/index.html"
+  },
+  {
+    "revision": "80196a798195de928085",
+    "url": "/react-ENablex/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "be9fabe19d3c40d56bcb",
+    "url": "/react-ENablex/static/css/main.06e3d0ab.chunk.css"
+  },
+  {
+    "revision": "80196a798195de928085",
+    "url": "/react-ENablex/static/js/2.1a280da7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ENablex/static/js/2.1a280da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9fabe19d3c40d56bcb",
+    "url": "/react-ENablex/static/js/main.b3c9a171.chunk.js"
+  },
+  {
+    "revision": "65fb18e3ec7943a71965",
+    "url": "/react-ENablex/static/js/runtime-main.e43e62cc.js"
+  }
+]);
